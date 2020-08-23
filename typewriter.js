7f37b0cb07895e114d3eb1e3e4d37868bf5ea0ec
Thai Do <thai.hl.do@gmail.com>
2020-08-23 22:30:40 +0000

    got rid of some unused variables

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,9 @@
 const sentence = "hello there from lighthouse labs";
 
 let delay = 0; //1, 2
-let results = ""
-
 for(const char of sentence) {
   setTimeout(() => {
-    results = process.stdout.write(char);
+    process.stdout.write(char);
     delay += 50;
   }, delay)
 };

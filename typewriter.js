04e44dfbe16bab45d78a8c608807091119bad1d6
Thai Do <thai.hl.do@gmail.com>
2020-08-23 22:55:29 +0000

    fixed the function so it actually prints out an animation

--- typewriter.js
+++ typewriter.js
@@ -4,8 +4,7 @@ let delay = 0; //1, 2
 for(const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
-    delay += 50;
-  }, delay)
+  }, delay += 50)
 };
 
 setTimeout(() => {
